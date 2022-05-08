5a07fc9f615d2751946a8157f17a80c1e313e169
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-05-07 20:34:47 -0400

    Removed extra files and made placed output in div wrappers

---
+++ primenumber-frontend/src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  
+  const [valid, setValid] = useState(true);
+  const [number, setNumber] = useState("Enter a number here");
+  const [result, setResult] = useState(" ");
+
+
+  function isPrimeNumber(input) {
+    if(isNaN(input) || input.length === 0)
+    {
+      console.log(input + " is NaN")
+      setResult(" ");
+      setValid(false);
+      return;
+    }
+
+    // return 0 here if the number is not prime
+    for(let i = 2; i < parseInt(input); i++)
+    {
+      if(parseInt(input) % i === 0)
+      {
+        setResult("NOT PRIME");
+        return;
+      }
+    }
+
+    setResult("PRIME");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    isPrimeNumber(number)
+  }
+
+  function handleChange(e) {
+    setValid(true)
+    setResult(" ")
+    setNumber(e.target.value);
+  }
+  
+  return (
+    <>
+    <div className="App">
+
+      <div className='header'><p>Prime number calculator</p></div>
+
+      <div className='dialog'>
+        <p>
+          {valid ? ' ' : 'Please enter a number.'}
+        </p>
+        <p>
+          {result}
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input className='numberInput' type="text" placeholder={number} onChange={handleChange} />
+        </label>
+        <br></br>
+        <input className='check-btn' type="submit" value="Check" />
+      </form>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
